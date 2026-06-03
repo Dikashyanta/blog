ef3348e6272c6a08082545bc0bd044b291f699a7
Dikashyanta <dikshyanta10@gmail.com>
2026-06-03 18:55:16 +0545

    Add Blog model and update admin registration for categories

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import category
+from .models import category, Blog
 
 # Register your models here.
 admin.site.register(category)
+
+admin.site.register(Blog)
+
--- blogs/models.py
+++ blogs/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -12,4 +13,25 @@ class category(models.Model):
 
     def __str__(self):
         return self.category_name
+
+STATUS_CHOICES = (
+    (0, 'Draft'),
+    (1, 'Published'),
+)
+
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    category = models.ForeignKey(category, on_delete=models.CASCADE)
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    featured_image = models.ImageField(upload_to='uploads/%Y/%m/%d/')
+    short_description = models.CharField(max_length=500)
+    blog_body = models.TextField(max_length=5000)
+    status = models.IntegerField(choices=STATUS_CHOICES, default=0) 
+    is_featured = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
 
