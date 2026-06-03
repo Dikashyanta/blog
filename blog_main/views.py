914b531e94ae6e891fa1ef240e38ae2ce14ee042
Dikashyanta <dikshyanta10@gmail.com>
2026-06-03 21:33:54 +0545

    Enhance home view to display categories and featured posts with updated template

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,7 +1,15 @@
 
 
 from django.shortcuts import render
+from blogs.models import Blog, category
 
 
 def home(request):
-    return render(request, 'home.html')
+    categories = category.objects.all()
+    featured_posts = Blog.objects.filter(is_featured=True, status='published').order_by('-created_at')[:5]
+    context = {
+        'categories': categories,
+        'featured_posts': featured_posts
+    }
+
+    return render(request, 'home.html', context)
