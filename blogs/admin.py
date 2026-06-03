d84fc9d7609b3017d8cf68d3ff0d714bb6e3c41d
Dikashyanta <dikshyanta10@gmail.com>
2026-06-03 20:30:48 +0545

    Refactor Blog model to use string choices for status and update admin configuration

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 from .models import category, Blog
 
+class blogAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+    list_display = ('title', 'author', 'status', 'is_featured', 'created_at', 'category') 
+    search_fields = ('title', 'author__username','id', 'category__category_name', 'status')
+    list_editable = ('status', 'is_featured') 
+
+
 # Register your models here.
 admin.site.register(category)
 
-admin.site.register(Blog)
+admin.site.register(Blog, blogAdmin)
 
