ae2ca1c3e853a56f83d48e8a562e3009577bdb85
Dikashyanta <dikshyanta10@gmail.com>
2026-06-10 21:07:54 +0545

    Add dashboards app with views, templates, and URL routing; update base template for dashboard link

--- blog_main/urls.py
+++ blog_main/urls.py
@@ -32,7 +32,12 @@ urlpatterns = [
 
     path('register/', views.register, name='register'),
     path('login/', views.login, name='login'),
-    path('logout/', views.logout, name='logout')
+    path('logout/', views.logout, name='logout'),
+
+    #Dashboard
+    path('dashboard/', include('dashboards.urls')),
+
+   
     
 
 
