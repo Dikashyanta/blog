f1850cdc382c5e49cff75a078c08297dd91d0335
Dikashyanta <dikshyanta10@gmail.com>
2026-06-12 22:24:33 +0545

    Add category management features; implement add and edit category views, forms, and templates; update URLs and enhance categories display

---
+++ dashboards/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from blogs.models import category
+
+
+
+
+class categoryForm(forms.ModelForm):
+    class Meta:
+        model = category
+        fields = '__all__'
+
+        
--- dashboards/views.py
+++ dashboards/views.py
@@ -1,13 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from blogs.models import category, Blog
 from django.contrib.auth.decorators import login_required
+from .forms import categoryForm
 
 
 @login_required(login_url='login')
 def dashboard(request):
     category_count = category.objects.all().count()
     blogs_count = Blog.objects.all().count()
-   
 
     context = {
         
@@ -18,10 +18,31 @@ def dashboard(request):
     return render(request, 'dashboard/dashboard.html', context)
 
 def categories(request):
-    return render(request, 'dashboard/categories.html')
+    categories_list = category.objects.all()
+    context = {
+        'categories': categories_list,
+    }
+    return render(request, 'dashboard/categories.html', context)
 
 def add_category(request):
-    return render(request, 'dashboard/add_category.html')
+    if request.method == 'POST':
+        form = categoryForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('categories')
+    else:
+        form = categoryForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'dashboard/add_category.html', context)
+
+def edit_category(request, pk):
+    form = categoryForm()
+    context ={
+        'form':form,
+    }
+    return render(request, 'dashboard/edit_category.html', context)
 
 
 
